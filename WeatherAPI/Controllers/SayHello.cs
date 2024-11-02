0a9d1342de4de6c482d21111a96d185f20273ff2
Lucas Aguiar <7lucasdaniel@gmail.com>
2024-11-01 21:19:23 -0300

    adicionando controller say hello

---
+++ WeatherAPI/Controllers/SayHello.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SayHello : ControllerBase
+    {
+        [HttpGet("SayHello")]
+        public IActionResult GetSayHello()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
